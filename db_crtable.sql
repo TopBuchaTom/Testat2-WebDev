b0201d18626e61ed3150da2a89e6d057315227f6
TopBuchaTom <tzacher2012@outlook.de>
2023-06-02 11:44:29 +0200

    TZ

--- db_crtable.sql
+++ db_crtable.sql
@@ -3,3 +3,13 @@ CREATE TABLE grusskarten (
     card LONGTEXT,
     lastModified  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+/*
+Bevor man den GrußkartenEditor startet, muss man eine Datenbank anlegen:
+- XAMPP Conrtrol Panel öffnen --> Webserver & MySQL starten
+ --> Auf Admin bei MySQL klicken oder http://localhost:8080/phpmyadmin/ in Browser eingeben
+  -->  auf Datenbanken klicken
+   --> DB mit dem Namen "grußkarten" anlegen
+    --> auf SQL klicken & den oben stehenden Code einfügen
+     --> auf ok klicken, dann kann's los gehen
+*/
